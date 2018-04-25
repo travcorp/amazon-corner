27f26e194d89ba0baa8efd6e20bdc02891615670
HirakN <hiraknaik.hn@gmail.com>
2018-04-25 17:03:03 +0100

    remove multiple RUN commands in dockerfile

---
+++ cloud-formation/scripts/check-deprecated-ami/ami-check.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# echo "hello"
+
+echo fetching available images
+aws ec2 describe-images --filters "Name=platform, Values=windows" --query 'Images[*].{ID:ImageId}' > all.txt
+
+echo fetching images used by running instances
+aws ec2 describe-instances --query "Reservations[*].Instances[*].[ImageId]" --filters "Name=platform, Values=windows" > inuse.txt 
+
+echo finding deprecated images
+grep -v -f all.txt inuse.txt | sort -u > deprecated.txt
+
+echo cleaning up
+rm all.txt inuse.txt
+cat deprecated.txt
