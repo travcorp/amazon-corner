e8fdea1dd3a803e32f39e74237c8892be7d0a6cb
HirakN <hiraknaik.hn@gmail.com>
2018-04-27 16:19:01 +0100

    added a script to build docker

--- cloud-formation/scripts/check-deprecated-ami/ami-check.sh
+++ cloud-formation/scripts/check-deprecated-ami/ami-check.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-# echo "hello"
+AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
 
-echo fetching available images
-aws ec2 describe-images --filters "Name=platform, Values=windows" --query 'Images[*].{ID:ImageId}' > all.txt
+echo fetching available images...
+aaws ec2 describe-images --filters "Name=platform, Values=windows" --query 'Images[*].{ID:ImageId}' > all.txt
 
-echo fetching images used by running instances
-aws ec2 describe-instances --query "Reservations[*].Instances[*].[ImageId]" --filters "Name=platform, Values=windows" > inuse.txt 
+echo fetching images used by running instances...
+aaws ec2 describe-instances --query "Reservations[*].Instances[*].[ImageId]" --filters "Name=platform, Values=windows" > inuse.txt 
 
-echo finding deprecated images
+echo finding deprecated images...
 grep -v -f all.txt inuse.txt | sort -u > deprecated.txt
 
-echo cleaning up
-rm all.txt inuse.txt
 cat deprecated.txt
+echo cleaning up...
+rm all.txt inuse.txt deprecated.txt
