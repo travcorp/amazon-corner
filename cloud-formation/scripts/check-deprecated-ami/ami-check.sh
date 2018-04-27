01af1010055ee3c0dc8847ecd5fce1140e129f30
HirakN <hiraknaik.hn@gmail.com>
2018-04-27 17:49:24 +0100

    wip

--- cloud-formation/scripts/check-deprecated-ami/ami-check.sh
+++ cloud-formation/scripts/check-deprecated-ami/ami-check.sh
@@ -3,14 +3,11 @@ AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
 
 echo seting region...
 aws configure set region eu-west-1
-aws configure get region
-cat ~/.aws/config
-
-echo seting region as envvar...
 export AWS_DEFAULT_REGION=us-west-1
 
 echo fetching available images...
 aws ec2 describe-images --filters "Name=platform, Values=windows" --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
+cat all.txt
 
 echo fetching images used by running instances...
 aws ec2 describe-instances --query "Reservations[*].Instances[*].[ImageId]" --filters "Name=platform, Values=windows" --output text --profile $AWS_PROFILE > inuse.txt 
