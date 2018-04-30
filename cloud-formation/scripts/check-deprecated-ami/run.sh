d7f74555f7b6310e36d96914d8bb9d0c69a9fa1c
Nigel Taylor <nigel.taylor@travcorpuk.com>
2018-04-30 13:43:12 +0100

    Update run.sh

--- cloud-formation/scripts/check-deprecated-ami/run.sh
+++ cloud-formation/scripts/check-deprecated-ami/run.sh
@@ -3,6 +3,11 @@ set -exu
 
 agent_name=`echo "$teamcity_agent_name" | tr '[:upper:]' '[:lower:]'`
 
+echo test this > /data/teamcity_agents/$agent_name/work/$(basename "$PWD")/myfile.txt
+echo myfile is ... 
+cat /data/teamcity_agents/$agent_name/work/$(basename "$PWD")/myfile.txt
+
+
 docker build -t ami-check .
 
 echo 'running container'
