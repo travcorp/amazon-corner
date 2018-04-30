8ce4bbb1570841311e92a3aab28eac13fc5047a2
Nigel Taylor <nigel.taylor@travcorpuk.com>
2018-04-30 10:43:01 +0100

    Update run.sh

--- cloud-formation/scripts/check-deprecated-ami/run.sh
+++ cloud-formation/scripts/check-deprecated-ami/run.sh
@@ -1,15 +1,13 @@
-#! /bin/bash
-if [ -z ${teamcity_agent_name} ]; then
-  build_dir=`pwd`
-else
-  agent_name=`echo "$teamcity_agent_name" | tr '[:upper:]' '[:lower:]'`
-  build_dir=/data/teamcity_agents/$agent_name/work/$(basename "$PWD")
-fi
+#!/bin/bash
+set -exu
+
+agent_name=`echo "$teamcity_agent_name" | tr '[:upper:]' '[:lower:]'`
 
 docker build -t ami-check .
 
 echo 'running container'
 docker run \
+	--cap-add SYS_ADMIN \
 	-e DEPLOY_ROLE_ARN=$DEPLOY_ROLE_ARN \
 	-v /data/teamcity_agents/$agent_name/work/$(basename "$PWD"):/build \
 	ami-check 
