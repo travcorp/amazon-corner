f5d298bc1184f59fa6d2481c45bf8c98ec0b30dd
HirakN <hiraknaik.hn@gmail.com>
2018-04-30 19:15:44 +0100

    wip

--- cloud-formation/scripts/check-deprecated-ami/run.sh
+++ cloud-formation/scripts/check-deprecated-ami/run.sh
@@ -4,7 +4,7 @@ set -exu
 agent_name=`echo "$teamcity_agent_name" | tr '[:upper:]' '[:lower:]'`
 
 
-MOUNTDIR=/data/teamcity_agents/$agent_name/work/check-deprecated-ami
+# MOUNTDIR=/data/teamcity_agents/$agent_name/work/check-deprecated-ami
 
 # echo myfile is at $MOUNTDIR 
 # echo this is what is in work...
@@ -15,7 +15,12 @@ MOUNTDIR=/data/teamcity_agents/$agent_name/work/check-deprecated-ami
 # echo checking dir exists ... we are at $PWD
 # ls $MOUNTDIR
 # echo hello world > $MOUNTDIR/myfile.txt
-
+if [ -z ${teamcity_agent_name} ]; then
+  build_dir=`pwd`
+else
+  agent_name=`echo "$teamcity_agent_name" | tr '[:upper:]' '[:lower:]'`
+  build_dir=/data/teamcity_agents/$agent_name/work/$(basename "$PWD")
+fi
 
 docker build -t ami-check .
 
@@ -23,5 +28,5 @@ echo 'running container'
 docker run \
 	--cap-add SYS_ADMIN \
 	-e DEPLOY_ROLE_ARN=$DEPLOY_ROLE_ARN \
-	-v $MOUNTDIR:/build \
+	-v $build_dir:/build \
 	ami-check 
