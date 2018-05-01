af813984ee54d6ceebebff143811a776a8310466
HirakN <hiraknaik.hn@gmail.com>
2018-05-01 17:15:08 +0100

    made requested changes

--- cloud-formation/scripts/check-deprecated-ami/ami-check.sh
+++ cloud-formation/scripts/check-deprecated-ami/ami-check.sh
@@ -6,10 +6,10 @@ aws configure set region $REGION
 export AWS_DEFAULT_REGION=$REGION
 
 echo fetching available images...
-aws ec2 describe-images --filters "Name=platform, Values=windows" --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
+aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
 
 echo fetching images used by running instances...
-aws ec2 describe-instances --region $REGION --query 'Reservations[*].Instances[*].[ImageId, Tags[?Key==`Project`].Value]' --filters "Name=platform, Values=windows" --output text --profile $AWS_PROFILE | paste - - > inuse.txt
+aws ec2 describe-instances --region $REGION --query 'Reservations[*].Instances[*].[ImageId, Tags[?Key==`Project`].Value]' --output text --profile $AWS_PROFILE | paste - - > inuse.txt
 
 echo finding deprecated images...
 grep -v -f all.txt inuse.txt | sort -u > ./build/deprecated.txt
