bcb88e9eb18da9e8a7cd9bf2a98294117bea5828
HirakN <hiraknaik.hn@gmail.com>
2018-08-08 17:55:58 +0100

    added more parameters to script

--- dynamodb-refresh/README.md
+++ dynamodb-refresh/README.md
@@ -10,7 +10,6 @@ This is designed to run on TeamCity as a build job.
 >**NOTE:** This will delete the table before you copy over new information, all old information will be lost. This functionality can be changed in the future.
 
 ## Limitations
-- This tool is designed to copy DynamoDB tables to and from the _same_ region. It is easy to change this but there is no need for this at the moment.
 - Existing table is deleted before copy. Again, this can be removed or changed to be optional, but it is not needed at the moment
 
 ## Running on Teamcity
@@ -24,24 +23,28 @@ Name the IAM user something specific to the use e.g. PROOJECTX-dynbamodb-refresh
 ### How to use
 To run on Teamcity, add this repo as a VCS root.
 
-You will need to add 5 environment variables to allow the build to work correctly:
+You will need to add some environment variables to allow the build to work correctly:
 
 |Item | Variable name|
 |-----|--------------|
 |AWS access key ID for source account (copy from)|SOURCE_AWS_KEY| 
 |AWS secret key for source account (copy from)| SOURCE_SECRET_KEY|
+|AWS region to copy table to|SOURCE_REGION|
+|Name of the table to copy from|SOURCE_TABLE_NAME|
 |See above (copy to)|DESTINATION_AWS_KEY|
 |See above (copy to)|DESTINATION_SECRET_KEY|
-|AWS region to copy table to and from|REGION|
-|Name of the table to copy from|TABLE_NAME|
+|AWS region to copy table to|DESTINATION_REGION|
+|Name of the table to copy to|DESTINATION_TABLE_NAME|
+
+>**NOTE:** If the destination table name is an existing table, it will delete it first, then copy. 
 
 The [docker-build](docker-build.sh) script needs to be run in the build step.
 >**NOTE:** You may have to make the script executable with 
 ```
-chmod +x <BUILD_SCRIPT>
+chmod +x <PATH_TO_BUILD_SCRIPT>
 ```
 >**NOTE:** You may have to specify path to shell to execute the script
 ```
-/bin/bash <BUILD_SCRIPT>
+/bin/bash <PATH_TO_BUILD_SCRIPT>
 ```
 
--- dynamodb-refresh/db-refresh.js
+++ dynamodb-refresh/db-refresh.js
@@ -1,5 +1,9 @@
 var copy = require('copy-dynamodb-table').copy
 var DynamoDB = require('aws-sdk/clients/dynamodb');
+/*
+
+*/
+
 
 // Check for parameters being set
 if(process.env.SOURCE_AWS_KEY && process.env.SOURCE_AWS_KEY && process.env.DESTINATION_AWS_KEY && process.env.DESTINATION_SECRET_KEY) { 
@@ -14,7 +18,7 @@ else {
 // Deleting the existing dynamodb table to make way for a new one
 var destinationdynamodb = new DynamoDB({
     apiVersion: '2012-08-10',
-    region: 'eu-west-1', 
+    region: process.env.REGION, 
     credentials: {
       accessKeyId: process.env.DESTINATION_AWS_KEY,
       secretAccessKey: process.env.DESTINATION_SECRET_KEY
@@ -23,7 +27,7 @@ var destinationdynamodb = new DynamoDB({
 );
 
 var params = {
-  TableName: process.env.TABLE_NAME
+  TableName: process.env.DESTINATION_TABLE_NAME
 };
 
 console.log("Deleting existing table");
@@ -40,29 +44,28 @@ destinationdynamodb.waitFor('tableNotExists', params, function(err, data) {
 
 // Copy table cross account
 function copyTable() {
-  console.log("Copying table");
   // Credentials object for table you are copying FROM
   var sourceAWSConfig = {
     accessKeyId: process.env.SOURCE_AWS_KEY,
     secretAccessKey: process.env.SOURCE_SECRET_KEY,
-    region: process.env.REGION
+    region: process.env.SOURCE_REGION
   }
 
   // Credentials object for table you are copying TO
   var destinationAWSConfig = {
     accessKeyId: process.env.DESTINATION_AWS_KEY,
     secretAccessKey: process.env.DESTINATION_SECRET_KEY,
-    region: 'eu-west-1' // support cross zone copying
+    region: process.env.DESTINATION_REGION // support cross zone copying
   }
 
   // Copies table over from source to dest.
   copy({
     source: {
-      tableName: process.env.TABLE_NAME, // required
+      tableName: process.env.SOURCE_TABLE_NAME, // required
       config: sourceAWSConfig // optional , leave blank to use globalAWSConfig
     },
     destination: {
-      tableName: process.env.TABLE_NAME, // required
+      tableName: process.env.DESTINATION_TABLE_NAME, // required
       config: destinationAWSConfig // optional , leave blank to use globalAWSConfig      
     },
     log: true,// default false
@@ -72,6 +75,7 @@ function copyTable() {
       if (err) {
         console.log(err)
       }
+      console.log("Copying table");
       console.log(result)
     }
   );
