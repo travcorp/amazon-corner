e66109d5fbba842f68bccf5eb138448f8c30dbbb
HirakN <hiraknaik.hn@gmail.com>
2019-10-04 18:22:23 +0100

    Changed to table output

--- check-deprecated-ami/ami-check.sh
+++ check-deprecated-ami/ami-check.sh
@@ -1,12 +1,10 @@
 #! /bin/bash
 AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
-# AWS_PROFILE=dev
-
-cd build
 
 echo seting region...
-# aws configure set region $REGION
 export AWS_DEFAULT_REGION=$REGION
+
+cd build
 
 echo fetching available images...
 aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
@@ -15,10 +13,10 @@ echo fetching images used by running instances...
 aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`].Value | [0],Instance:InstanceId, Project:Tags[?Key==`Project`].Value | [0]}' --output table --profile $AWS_PROFILE > inuse.txt
 
 echo finding deprecated images...
-grep -v -f all.txt inuse.txt > deprecated.txt
-(head -n 5 deprecated.txt && tail -n +6 deprecated.txt | sort -rk3) > d.txt
+grep -v -f all.txt inuse.txt > unsorted_deprecated.txt
+(head -n 5 unsorted_deprecated.txt && tail -n +6 unsorted_deprecated.txt | sort -rk3) > deprecated.txt
 
-# if [ -s deprecated.txt ]; then
-# 	echo DEPRECATED AMIS FOUND
-# 	exit 1
-# fi
+if [ -s deprecated.txt ]; then
+	echo DEPRECATED AMIS FOUND
+	exit 1
+fi
