85d950f7f078ed931ac6039883231236b6f298f7
HirakN <hiraknaik.hn@gmail.com>
2019-10-04 17:57:10 +0100

    wip

--- check-deprecated-ami/ami-check.sh
+++ check-deprecated-ami/ami-check.sh
@@ -12,13 +12,13 @@ echo fetching available images...
 aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
 
 echo fetching images used by running instances...
-# aws ec2 describe-instances --query 'Reservations[*].Instances[*].[ImageId, InstanceId, Tags[?Key==`Project`].Value | [0]]' --output text --profile $AWS_PROFILE > inuse.txt
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],ami:ImageId, name:Tags[?Key==`Name`].Value | [0],InstanceId:InstanceId, Project:Tags[?Key==`Project`].Value | [0]}' --output table --profile $AWS_PROFILE > inuse.txt
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`].Value | [0],Instance:InstanceId, Project:Tags[?Key==`Project`].Value | [0]}' --output table --profile $AWS_PROFILE > inuse.txt
+
 echo finding deprecated images...
 grep -v -f all.txt inuse.txt > deprecated.txt
 (head -n 5 deprecated.txt && tail -n +6 deprecated.txt | sort -rk3) > d.txt
 
-if [ -s deprecated.txt ]; then
-	echo DEPRECATED AMIS FOUND
-	exit 1
-fi
+# if [ -s deprecated.txt ]; then
+# 	echo DEPRECATED AMIS FOUND
+# 	exit 1
+# fi
