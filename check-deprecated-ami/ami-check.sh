24ecc9e0b142b203cbae6d377f450d8e63b84158
HirakN <hiraknaik.hn@gmail.com>
2019-10-14 14:36:55 +0100

    changed profile

--- check-deprecated-ami/ami-check.sh
+++ check-deprecated-ami/ami-check.sh
@@ -11,7 +11,8 @@ echo fetching available images...
 aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
 
 echo fetching images used by running instances...
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`] | [0].Value,Instance:InstanceId, Project:Tags[?Key==`Project`] | [0].Value} | [] | sort_by(@,&to_string(Project))' --output table --profile dev > inuse.txt
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`] | [0].Value,Instance:InstanceId, Project:Tags[?Key==`Project`] | [0].Value} | [] | sort_by(@,&to_string(Project))' --output table --profile $AWS_PROFILE > inuse.txt
+
 echo finding deprecated images...
 grep -v -f all.txt inuse.txt > deprecated.txt
 
