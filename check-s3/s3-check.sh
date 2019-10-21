8f4df9e50307efd32f079560b1b13dd8f8e2b1fd
HirakN <hiraknaik.hn@gmail.com>
2019-10-21 17:45:04 +0100

    wip

--- check-s3/s3-check.sh
+++ check-s3/s3-check.sh
@@ -1,28 +1,31 @@
 #! /bin/bash
-AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
+# AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
+AWS_PROFILE=dev
 
-cd build
+# echo setting region...
+# export AWS_DEFAULT_REGION=$REGION
 
-echo setting region...
-aws configure set region $REGION
-export AWS_DEFAULT_REGION=$REGION
-
-echo fetching s3 buckets and latest object
+echo fetching s3 buckets and latest object...
 
 for BUCKET in `aws --profile $AWS_PROFILE s3 ls | cut -d' ' -f 3` ;
 do
-	echo bucket is $BUCKET
+	echo checking $BUCKET...
+
+	LAST_MOD=$(aws s3api list-objects-v2 --bucket $BUCKET --query 'Contents[?LastModified<=`2018-12-31`][LastModified, Key] | sort_by(@,&[0]) | [0]' --profile dev --output text)
 
         #aws s3 ls $BUCKET --recursive | sort | tail -n 1 | awk '{print $4}'
-        LINE=`aws --profile $AWS_PROFILE s3 ls $BUCKET --recursive | sort | tail -n 1 `
+        # LINE=`aws --profile $AWS_PROFILE s3 ls $BUCKET --recursive | sort | tail -n 1 `
+	if [[ $LAST_MOD == "None" ]]; then
+		RESULT="no old files" 
+	elif [[ $LAST_MOD == "" ]]; then
+		RESULT="empty bucket"
+	fi
+	
+	echo checked bucket $BUCKET and found $RESULT >> output.log
 
-        echo checked bucket $BUCKET and found $LINE >> out
-
-	#break
-
+	echo $BUCKET $LAST_MOD >> buckets.old
 done
 
-echo Done
+# echo Done
 
-cat out  | grep checked | sed 's/  / /g' | awk '{print $6, $3 }' | grep 20 | sort  | head -n 50 > oldestBuckets.txt
-
+# cat out  | grep checked | sed 's/  / /g' | awk '{print $6, $3 }' | grep 20 | sort  | head -n 50 > oldestBuckets.txt
