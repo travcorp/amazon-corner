c8ead33a64d2f3a00774690baba26ac9257c18f9
HirakN <hiraknaik.hn@gmail.com>
2019-10-25 17:30:45 +0100

    s3 fix

--- check-s3/s3-check.sh
+++ check-s3/s3-check.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
-# AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
-AWS_PROFILE=dev
+AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
 
-# echo setting region...
-# export AWS_DEFAULT_REGION=$REGION
+echo setting region...
+export AWS_DEFAULT_REGION=$REGION
 
 echo fetching s3 buckets and latest object...
 
-for BUCKET in `aws --profile $AWS_PROFILE s3 ls | cut -d' ' -f 3 | head` ;
+for BUCKET in `aws --profile $AWS_PROFILE s3 ls | cut -d' ' -f 3` ;
 do
 	echo checking $BUCKET...
 
-	# LAST_MOD=$(aws s3api list-objects-v2 --bucket $BUCKET --query 'Contents[?LastModified<=`2018-12-31`][LastModified, Key] | sort_by(@,&[0]) | [0]' --profile dev --output text)
 	LAST_MOD=$(aws s3api list-objects-v2 --query 'Contents[?LastModified<=`2018-12-31`][LastModified, Key] | sort_by(@,&[0]) | [0] | [0]' --profile dev --output json --bucket $BUCKET 2>/dev/null)
 	
 	if [[ $LAST_MOD == "null" ]]; then
