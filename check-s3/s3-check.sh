38efd7e0775e259ba837cbad107a32ad7450e519
HirakN <hiraknaik.hn@gmail.com>
2019-10-28 18:11:12 +0000

    changed the last modifed

--- check-s3/s3-check.sh
+++ check-s3/s3-check.sh
@@ -10,16 +10,17 @@ for BUCKET in `aws --profile $AWS_PROFILE s3 ls | cut -d' ' -f 3` ;
 do
 	echo checking $BUCKET...
 	
-	LAST_MOD=$(aws s3api list-objects-v2 --query 'Contents[?LastModified<=`2018-12-31`][LastModified, Key] | sort_by(@,&[0]) | [0] | [0]' --profile $AWS_PROFILE --output json --bucket $BUCKET 2>/dev/null)
+	# Check for files newer than 2018-12-31
+	LAST_MOD=$(aws s3api list-objects-v2 --query 'Contents[?LastModified>=`2018-12-31`][LastModified, Key] | sort_by(@,&[0]) | [-1] | [0]' --profile $AWS_PROFILE --output json --bucket $BUCKET 2>/dev/null)
 	
 	if [[ $LAST_MOD == "null" ]]; then
-		OUTPUT="no old files" 
+		OUTPUT="OLD"
+		echo $BUCKET $OUTPUT >> dirty-old-buckets.txt
 	elif [[ $LAST_MOD == "" ]]; then
 		OUTPUT="empty"
 		echo $BUCKET $OUTPUT >> dirty-old-buckets.txt
 	else
-		echo $BUCKET $LAST_MOD >> dirty-old-buckets.txt
-		OUTPUT=$LAST_MOD
+		OUTPUT="recent file $LAST_MOD"
 	fi
 	
 	echo checked bucket $BUCKET and found $OUTPUT >> output.txt
