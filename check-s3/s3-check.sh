99c28ddaa98f5fbf02620a485670959721ee1c88
Hirak Naik <34275224+HirakN@users.noreply.github.com>
2019-10-30 17:29:49 +0000

    s3 old files check update (#10)
    * wip
    
    * s3 fix
    
    * added bsdutils
    
    * wip
    
    * fix
    
    * work
    
    * changed the last modifed

--- check-s3/s3-check.sh
+++ check-s3/s3-check.sh
@@ -1,28 +1,36 @@
 #! /bin/bash
 AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
 
-cd build
-
 echo setting region...
-aws configure set region $REGION
 export AWS_DEFAULT_REGION=$REGION
 
-echo fetching s3 buckets and latest object
+echo fetching s3 buckets and latest object...
 
 for BUCKET in `aws --profile $AWS_PROFILE s3 ls | cut -d' ' -f 3` ;
 do
-	echo bucket is $BUCKET
-
-        #aws s3 ls $BUCKET --recursive | sort | tail -n 1 | awk '{print $4}'
-        LINE=`aws --profile $AWS_PROFILE s3 ls $BUCKET --recursive | sort | tail -n 1 `
-
-        echo checked bucket $BUCKET and found $LINE >> out
-
-	#break
+	echo checking $BUCKET...
+	
+	# Check for files newer than 2018-12-31
+	LAST_MOD=$(aws s3api list-objects-v2 --query 'Contents[?LastModified>=`2018-12-31`][LastModified, Key] | sort_by(@,&[0]) | [-1] | [0]' --profile $AWS_PROFILE --output json --bucket $BUCKET 2>/dev/null)
+	
+	if [[ $LAST_MOD == "null" ]]; then
+		OUTPUT="OLD"
+		echo $BUCKET $OUTPUT >> dirty-old-buckets.txt
+	elif [[ $LAST_MOD == "" ]]; then
+		OUTPUT="empty"
+		echo $BUCKET $OUTPUT >> dirty-old-buckets.txt
+	else
+		OUTPUT="recent file $LAST_MOD"
+	fi
+	
+	echo checked bucket $BUCKET and found $OUTPUT >> output.txt
 
 done
 
+echo formatting output...
+cat dirty-old-buckets.txt | column -t > old-buckets.txt
+
+echo moving artifacts...
+cp old-buckets.txt output.txt /build
+
 echo Done
-
-cat out  | grep checked | sed 's/  / /g' | awk '{print $6, $3 }' | grep 20 | sort  | head -n 50 > oldestBuckets.txt
-
