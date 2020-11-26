6baeee798a8f9917799394129c3430100fe48872
Hanad Ahmed <hanad15@hotmail.co.uk>
2020-11-26 11:46:38 +0000

    wip

--- check-deprecated-ami/ami-check.sh
+++ check-deprecated-ami/ami-check.sh
@@ -5,7 +5,7 @@ set -e
 echo seting region...
 # export AWS_DEFAULT_REGION=$REGION
 
-AWS_PROFILE=$(aws sts assume-role --role-arn $DEPLOY_ROLE_ARN --role-session-name CLI-SESSION)
+PROFILE=$(aws sts assume-role --role-arn $DEPLOY_ROLE_ARN --role-session-name CLI-SESSION)
 
 export AWS_ACCESS_KEY_ID=$(echo $TEMP_ROLE | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $TEMP_ROLE | jq .Credentials.SecretAccessKey | xargs)
@@ -14,10 +14,10 @@ export AWS_SESSION_TOKEN=$(echo $TEMP_ROLE | jq .Credentials.SessionToken | xarg
 cd build
 
 echo fetching available images...
-aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
+aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $PROFILE > all.txt
 
 echo fetching images used by running instances...
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`] | [0].Value,Instance:InstanceId, Project:Tags[?Key==`Project`] | [0].Value} | [] | sort_by(@,&to_string(Project))' --output table --profile $AWS_PROFILE > inuse.txt
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`] | [0].Value,Instance:InstanceId, Project:Tags[?Key==`Project`] | [0].Value} | [] | sort_by(@,&to_string(Project))' --output table --profile $PROFILE > inuse.txt
 
 echo finding deprecated images...
 grep -v -f all.txt inuse.txt > deprecated.txt
