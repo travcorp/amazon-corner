738c4d598f7c6f46d9b1deb87c9a72ab161fddbd
S-Stefan <45593951+S-Stefan@users.noreply.github.com>
2020-11-27 14:49:31 +0000

    Check ami fix (#12)
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    Co-authored-by: Hanad Ahmed <hanad15@hotmail.co.uk>

--- check-deprecated-ami/ami-check.sh
+++ check-deprecated-ami/ami-check.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 set -e
-AWS_PROFILE=`. /aws-assume-role.sh $DEPLOY_ROLE_ARN`
+
+echo seting user profile...
+PROFILE=$(aws sts assume-role --role-arn $DEPLOY_ROLE_ARN --role-session-name CLI-SESSION)
+export AWS_ACCESS_KEY_ID=$(echo $PROFILE | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $PROFILE | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo $PROFILE | jq .Credentials.SessionToken | xargs)
 
 echo seting region...
 export AWS_DEFAULT_REGION=$REGION
@@ -8,10 +13,10 @@ export AWS_DEFAULT_REGION=$REGION
 cd build
 
 echo fetching available images...
-aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text --profile $AWS_PROFILE > all.txt
+aws ec2 describe-images --query 'Images[*].{ID:ImageId}' --output text > all.txt
 
 echo fetching images used by running instances...
-aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`] | [0].Value,Instance:InstanceId, Project:Tags[?Key==`Project`] | [0].Value} | [] | sort_by(@,&to_string(Project))' --output table --profile $AWS_PROFILE > inuse.txt
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].{Project:Tags[?Key==`Project`].Value | [0],AMI:ImageId, Name:Tags[?Key==`Name`] | [0].Value,Instance:InstanceId, Project:Tags[?Key==`Project`] | [0].Value} | [] | sort_by(@,&to_string(Project))' --output table > inuse.txt
 
 echo finding deprecated images...
 grep -v -f all.txt inuse.txt > deprecated.txt
